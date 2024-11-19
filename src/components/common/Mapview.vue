66ac186d2864d6b2c753e0c1e5f6764071e235bc
ZY <zeng-yang@qq.com>
2024-11-19 21:27:40 +0800

    增加vscod配置

---
+++ src/components/common/Mapview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="mapview"></div>
+</template>
+
+<script>
+export default {
+  name: 'mapview',
+  components: {},
+}
+</script>
+
+<style>
+#mapview {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
